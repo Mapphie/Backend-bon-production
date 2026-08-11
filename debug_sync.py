981b12ec19c1882ab1eafeb04d9ceba70afbad9a
Mapphie <maaphlixx@gmail.com>
2026-08-11 17:16:49 +0300

    connexion base ok

---
+++ debug_sync.py
@@ -0,0 +1,6 @@
+from database import sync_engine
+from sqlalchemy import text
+
+with sync_engine.connect() as conn:
+    result = conn.execute(text("SELECT DB_NAME() AS current_db"))
+    print("Connexion réussie à :", result.fetchone())
